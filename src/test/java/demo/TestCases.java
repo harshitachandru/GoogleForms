3004787235136004fedb13ff5ca24f8cdf2e4a3e
Harshita C <harshuhsn2468@gmail.com>
2024-09-13 13:47:53 +0530

    ME_QA_XGMAIL_CAL1_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,15 +1,19 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.logging.Level;
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
@@ -21,8 +25,6 @@ public class TestCases {
      * TODO: Write your tests here with testng @Test annotation. 
      * Follow `testCase01` `testCase02`... format or what is provided in instructions
      */
-
-     
     /*
      * Do not change the provided methods unless necessary, they will help in automation and assessment
      */
@@ -54,6 +56,82 @@ public class TestCases {
     {
         driver.close();
         driver.quit();
+    }
+
+    @Test
+    public void testCase01() throws InterruptedException
+    {
+        
+        driver.get("https://forms.gle/wjPkzeSEk1CM7KgGA");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        //Enter the Name
+        WebElement Nametext = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class,'aXBtI ')]//input[@type='text' and contains(@aria-labelledby,'i')]")));
+        Wrappers.movetotheelementtoclick(driver, Nametext);
+        Thread.sleep(4000);
+        System.out.println("Wait 1");
+        Wrappers.enterText(Nametext, "Crio Learner");
+
+        //Enter the Text
+        WebElement textarea = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//textarea[@class='KHxj8b tL9Q4c']")));
+        Wrappers.movetotheelementtoclick(driver, textarea);
+        String content = "I want to be the best QA Engineer! ";
+        String epoctimestring = Wrappers.epochtime();
+        Thread.sleep(2000);
+        System.out.println("Wait 2");
+        Wrappers.enterText(textarea, content+" "+epoctimestring);
+        
+
+        //Radio Button
+        Wrappers.radioselection(driver, "0 - 2");
+
+        //Checkbox selction
+        Wrappers.checkboxselection(driver, "Java");
+        Wrappers.checkboxselection(driver, "Selenium");
+        Wrappers.checkboxselection(driver, "TestNG");
+
+        //Click on the dropdown
+        WebElement maindropdown = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='ry3kXd']")));
+        Wrappers.movetotheelementtoclick(driver, maindropdown);
+        Thread.sleep(2000);
+        System.out.println("Wait 3");
+        Wrappers.dropdownselection(driver, "Mrs");
+
+
+        //Select the date
+        WebElement selectinputdate = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//input[@type='date']")));
+        String selectdatefrompastsevendays = Wrappers.returnupdatedlocaldate();
+        Wrappers.movetotheelementtoclick(driver, textarea);
+        // Thread.sleep(3000);
+        // System.out.println("Wait 4");
+        Wrappers.enterText(selectinputdate, selectdatefrompastsevendays);
+
+        //Enter the time
+        WebElement timeinHours = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='Xb9hP']//input[@aria-label='Hour']")));
+        WebElement timeinMin = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='Xb9hP']//input[@aria-label='Minute']")));
+
+        Wrappers.enterText(timeinHours, "07");
+        Wrappers.enterText(timeinMin, "30");
+        Thread.sleep(2000);
+
+        WebElement submit = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[text()='Submit']")));
+        submit.click();
+
+        WebElement successmessage1 = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@class='vHW8K']")));
+        String validatemessage = successmessage1.getText();
+        if(validatemessage.contains("Thanks for your response, Automation Wizard!"))
+        {
+            System.out.println("The success message is printed successfully: " + validatemessage);
+        }
+        else{
+            System.out.println("validation failed");
+        }
+
+        
 
     }
+
+
+
 }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -1,11 +1,13 @@
 package demo.wrappers;
 
 import java.time.Instant;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -16,4 +18,84 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+
+    public static void enterText(WebElement locator, String text) {
+        try {
+
+            locator.click();
+            locator.sendKeys(text);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void radioselection(ChromeDriver driver, String radiobuttonoption)
+    {
+        try {
+            
+            WebElement radiobutton = driver.findElement(By.xpath("//span[contains(@class,'aDTYNe ') and contains(text(),'" + radiobuttonoption + "')]/../../..//div[@class='vd3tt']"));
+            radiobutton.click();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void checkboxselection(ChromeDriver driver, String checkboxoption)
+    {
+        try {
+            
+            WebElement checkbox = driver.findElement(By.xpath("//span[contains(@class,'n5vBHf ') and contains(text(),'" + checkboxoption + "')]/../../preceding-sibling::div[@role='checkbox']"));
+            checkbox.click();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void dropdownselection(ChromeDriver driver, String dropdownoption)
+    {
+        try {
+            
+            WebElement dropdown = driver.findElement(By.xpath("//div[contains(@class,'QXL7Te')]//span[text()='" + dropdownoption + "']"));
+            dropdown.click();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void movetotheelementtoclick(ChromeDriver driver, WebElement locator)
+    {
+        try {
+            
+            JavascriptExecutor js = (JavascriptExecutor) driver;
+            js.executeScript("arguments[0].click();", locator);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String returnupdatedlocaldate()
+    {
+        LocalDate currentDate = LocalDate.now();
+        System.out.println("Current local date is : " + currentDate);
+
+        LocalDate updatedDate = currentDate.minusDays(7);
+        System.out.println("updated date is : " + updatedDate);
+
+        DateTimeFormatter formatform = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+        String formattedDate = updatedDate.format(formatform);
+
+        return formattedDate;
+    }
+
+    public static String epochtime()
+    {
+        long epochsystemtime = System.currentTimeMillis()/1000;
+        String epochtimeinstring = String.valueOf(epochsystemtime);
+        return epochtimeinstring;
+    }
 }
